9d45940782a6306e7e571858d276808c995c1fa0
emarchak <erin.marchak@shopify.com>
2017-11-26 14:14:17 -0500

    Create reducers

--- app/index.js
+++ app/index.js
@@ -1,6 +1,12 @@
 import ReactDOM from 'react-dom'
 import routes from './config/routes'
+import { createStore } from 'redux'
+import users from 'redux/modules/users'
 
+const store = createStore(users)
+
+
+console.log(store)
 ReactDOM.render(
   routes,
   document.getElementById('app')
